2f97fe56db6e9de32db3cef62bf84782e9366ae2
aleksandra <122663153+motspan-aleksandra@users.noreply.github.com>
2024-02-03 14:49:37 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let text = document.querySelector(".text");
+let btn = document.querySelector(".btn");
+let mylist = document.querySelector(".mylist__list");
+
+btn.addEventListener('click', () => {
+
+})
